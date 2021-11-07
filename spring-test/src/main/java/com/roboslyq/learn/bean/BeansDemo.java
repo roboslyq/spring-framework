dab744642741ff9c75e66187c3c95e71614b2be6
roboslyq <robosloveu@163.com>
2021-11-07 18:17:53 +0800

    调整本地配置，方便jar包下载，可以本地调试

---
+++ spring-test/src/main/java/com/roboslyq/learn/bean/BeansDemo.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C), 2015-2021
+ * FileName: BeansDemo
+ * Author:   roboslyq
+ * Date:     2021/11/6 23:48
+ * Description:
+ * History:
+ * <author>                 <time>          <version>          <desc>
+ * luo.yongqian         2021/11/6 23:48      1.0.0               创建
+ */
+package com.roboslyq.learn.bean;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ *
+ * 〈〉
+ * @author roboslyq
+ * @date 2021/11/6
+ * @since 1.0.0
+ */
+public class BeansDemo {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath://META-INF/beans.xml");
+		User user = context.getBean("user",User.class);
+		System.out.println(user.getClass());
+	}
+
+}
---
+++ spring-test/src/main/java/com/roboslyq/learn/bean/User.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2021
+ * FileName: User
+ * Author:   roboslyq
+ * Date:     2021/11/6 23:45
+ * Description:
+ * History:
+ * <author>                 <time>          <version>          <desc>
+ * luo.yongqian         2021/11/6 23:45      1.0.0               创建
+ */
+package com.roboslyq.learn.bean;
+
+/**
+ *
+ * 〈〉
+ * @author roboslyq
+ * @date 2021/11/6
+ * @since 1.0.0
+ */
+public class User {
+
+}
